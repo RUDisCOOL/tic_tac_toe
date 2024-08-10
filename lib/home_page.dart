e654af4f94735c85466b731cd9f414a37eebf721
RUDisCOOL <superrudra1601@gmail.com>
2024-08-11 00:51:50 +0530

    Improved Bot

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,7 +11,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
-  String _turn = 'X';
+  String player = 'X';
+  late String robot = player == 'X' ? 'O' : 'X';
+  late String _turn = player;
 
   int count = 0;
 
@@ -55,68 +57,49 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
         var (bool win, String? winner) = checkWinner(index);
         if (win) {
           _resetGame();
-          _showCustomDialog(
-            RichText(
-              text: TextSpan(
-                children: [
-                  const TextSpan(
-                    text: 'Player ',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black,
-                    ),
-                  ),
-                  WidgetSpan(
-                    child: Image.asset(
-                      'assets/images/$winner.png',
-                      height: 22,
-                      width: 22,
-                    ),
-                  ),
-                  const TextSpan(
-                    text: ' has ',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black,
-                    ),
-                  ),
-                  const TextSpan(
-                    text: 'Won!',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.green,
-                    ),
-                  ),
-                ],
+          if (winner == player) {
+            _showCustomDialog(
+              title: const Text(
+                'Congratulations!!',
+                style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold),
               ),
-            ),
-          );
+              message: const Text(
+                'You have WON the Game!!',
+                style: TextStyle(fontSize: 18),
+              ),
+            );
+          } else {
+            _showCustomDialog(
+              title: const Text(
+                'Better Luck Next Time!!',
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold),
+              ),
+              message: const Text(
+                'Oops! You have LOST the Game!!',
+                style: TextStyle(fontSize: 18),
+              ),
+            );
+          }
         }
         if (!win && count == 9) {
           _resetGame();
           _showCustomDialog(
-            RichText(
-              text: const TextSpan(
-                text: 'It\'s a ',
-                style: TextStyle(
+            title: const Text(
+              'Well Tried!!',
+              style: TextStyle(
+                  color: Color.fromRGBO(253, 216, 53, 1),
                   fontSize: 22,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black,
-                ),
-                children: [
-                  TextSpan(
-                    text: 'Draw!',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w600,
-                      color: Color.fromARGB(255, 255, 179, 0),
-                    ),
-                  ),
-                ],
-              ),
+                  fontWeight: FontWeight.bold),
+            ),
+            message: const Text(
+              'It\'s a DRAW!!',
+              style: TextStyle(fontSize: 18),
             ),
           );
         }
@@ -124,8 +107,10 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
       //BOT MOVE LOGIC HERE
       if (count < 9 && count % 2 != 0) {
         //Easy
-        int botIndex = possibleMoves[Random().nextInt(possibleMoves.length)];
+        // int botIndex = possibleMoves[Random().nextInt(possibleMoves.length)];
+
         //Medium
+        int botIndex = blockWin(rankList(possibleWins(index)));
 
         //Impossible
 
@@ -137,34 +122,101 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
     }
   }
 
+  int blockWin(Map<int, List<List<int>>> rankedList) {
+    int max = 0;
+    for (int key in rankedList.keys) {
+      if (max < key) {
+        max = key;
+      }
+    }
+    int index;
+    if (rankedList.isEmpty) {
+      index = possibleMoves[Random().nextInt(possibleMoves.length)];
+    } else {
+      List<int> closestWin = rankedList[max]![0];
+      index = closestWin[0];
+      for (index in closestWin) {
+        if (values[index] == '') {
+          break;
+        }
+      }
+    }
+    print(index);
+    return index;
+  }
+
+  Map<int, List<List<int>>> rankList(List<List<int>> possibleWinsList) {
+    Map<int, List<List<int>>> rankedList = {};
+    for (var list in possibleWinsList) {
+      int count = 0;
+      for (var index in list) {
+        if (values[index] == player) {
+          count++;
+        }
+      }
+      if (rankedList.containsKey(count)) {
+        rankedList[count]!.add(list);
+      } else {
+        rankedList[count] = [list];
+      }
+    }
+    return rankedList;
+  }
+
+  List<List<int>> possibleWins(int index) {
+    List<List<int>> possibleWinsList = [];
+    for (var list in winnerIndexList) {
+      if (list.contains(index)) {
+        bool isPossibleWin = true;
+        for (var i in list) {
+          if (values[i] == robot) {
+            isPossibleWin = false;
+            break;
+          }
+        }
+        if (isPossibleWin) possibleWinsList.add(list);
+      }
+    }
+    return possibleWinsList;
+  }
+
   void _resetGame() {
     setState(() {
       values = ['', '', '', '', '', '', '', '', ''];
       possibleMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-      _turn = 'X';
+      _turn = player;
       count = 0;
     });
   }
 
-  void _showCustomDialog(Widget message) {
+  void _showCustomDialog({Widget? title, required Widget message}) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-          title: message,
+          title: title,
+          content: message,
           actions: [
-            ElevatedButton(
-              style: const ButtonStyle(
-                backgroundColor:
-                    WidgetStatePropertyAll(Color.fromARGB(255, 13, 70, 117)),
-              ),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text(
-                'OK',
-                style: TextStyle(color: Colors.white),
+            Center(
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(
+                    Color.fromARGB(255, 13, 70, 117),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const SizedBox(
+                  width: double.infinity,
+                  child: Center(
+                    child: Text(
+                      'OK',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
@@ -212,57 +264,86 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
-          child: Container(
-            width: deviceHeight - 200,
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(7),
-                    color: const Color.fromARGB(255, 74, 74, 74),
-                  ),
-                  padding: const EdgeInsets.all(15),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    padding: const EdgeInsets.all(20),
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.pause_rounded,
+                      color: Color.fromARGB(255, 80, 80, 80),
                     ),
-                    clipBehavior: Clip.antiAlias,
-                    child: GridView.count(
-                      padding: EdgeInsets.zero,
-                      shrinkWrap: true,
-                      crossAxisCount: 3,
-                      mainAxisSpacing: 10,
-                      crossAxisSpacing: 10,
-                      children: List.generate(
-                        9,
-                        (int index) {
-                          return GestureDetector(
-                            onTap: () {
-                              _move(index);
+                    iconSize: 45,
+                  ),
+                  const Text(
+                    'TIC TAC TOE',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 80, 80, 80),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 85,
+                  )
+                ],
+              ),
+              Expanded(
+                child: Center(
+                  child: Container(
+                    width: deviceHeight - 200,
+                    padding: const EdgeInsets.only(
+                        left: 25, right: 25, top: 25, bottom: 25 + 85),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14),
+                        color: const Color.fromARGB(255, 80, 80, 80),
+                      ),
+                      padding: const EdgeInsets.all(20),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        clipBehavior: Clip.antiAlias,
+                        child: GridView.count(
+                          padding: EdgeInsets.zero,
+                          shrinkWrap: true,
+                          crossAxisCount: 3,
+                          mainAxisSpacing: 17,
+                          crossAxisSpacing: 17,
+                          children: List.generate(
+                            9,
+                            (int index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  if (count % 2 == 0) {
+                                    _move(index);
+                                  }
+                                },
+                                child: Container(
+                                  color: Colors.white,
+                                  child: Center(
+                                    child: values[index] == ''
+                                        ? const SizedBox()
+                                        : Image.asset(
+                                            'assets/images/${values[index]}.png'),
+                                  ),
+                                ),
+                              );
                             },
-                            child: Container(
-                              color: Colors.white,
-                              child: Center(
-                                child: values[index] == ''
-                                    ? const SizedBox()
-                                    : Image.asset(
-                                        'assets/images/${values[index]}.png'),
-                              ),
-                            ),
-                          );
-                        },
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
-                const SizedBox(
-                  height: 30,
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
